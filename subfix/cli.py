d4635990b898edbbcccc37ec33f5855537112e64
cronrpc <147160173+cronrpc@users.noreply.github.com>
2023-12-05 20:12:58 +0800

    cli init

--- subfix/cli.py
+++ subfix/cli.py
@@ -1,7 +1,7 @@
 import argparse
-from .webui import startwebui
 
 def handle_webui(args):
+    from .webui import startwebui
     startwebui(args)
 
 
@@ -13,6 +13,7 @@ def cli():
     parser = argparse.ArgumentParser(description="a tool to check or create TTS dataset")
     subparsers = parser.add_subparsers(dest='command')
     
+
     parser_webui = subparsers.add_parser('webui', 
                                           help='webui to modify audios')
     parser_webui.add_argument('--load_json', default="None", help='source file, like demo.json')
@@ -25,19 +26,34 @@ def cli():
     
 
     parser_create = subparsers.add_parser('create', 
-                                            help='create dataset by origin audio dirctory')
-    parser_create.add_argument("--source_dir", type=str, default="origin", help="Source directory path, Default: origin")
-    parser_create.add_argument("--target_dir", type=str, default="dataset", help="Target directory path, Default: dataset")
-    parser_create.add_argument("--resample_dir", type=str, default="origin_resample", help="Resample directory path, Default: origin_resample")
-    parser_create.add_argument("--sample_rate", type=int, default=44100, help="Sample rate, Default: 44100")
-    parser_create.add_argument("--language", type=str, default="ZH", help="Language, Default: ZH")
-    parser_create.add_argument("--output", type=str, default="demo.list", help="List file, Default: demo.list")
-    parser_create.add_argument("--max_seconds", type=int, default=15, help="Max sliced voice length(seconds), Default: 15")
-    parser_create.set_defaults(func=handle_create)
+                                          help='create dataset by origin audio dirctory')
+    create_subparsers = parser_create.add_subparsers(dest='solution', 
+                                                     help='auto asr solution, modelscope or whisper')
+
+    modelscope_subparsers = create_subparsers.add_parser('modelscope', 
+                                                         help='modelscope models')
+    modelscope_subparsers.add_argument("--source_dir", type=str, default="origin", help="Source directory path, Default: origin")
+    modelscope_subparsers.add_argument("--target_dir", type=str, default="dataset", help="Target directory path, Default: dataset")
+    modelscope_subparsers.add_argument("--resample_dir", type=str, default="origin_resample", help="Resample directory path, Default: origin_resample")
+    modelscope_subparsers.add_argument("--sample_rate", type=int, default=44100, help="Sample rate, Default: 44100")
+    modelscope_subparsers.add_argument("--language", type=str, default="ZH", help="Language, Default: ZH")
+    modelscope_subparsers.add_argument("--output", type=str, default="demo.list", help="List file, Default: demo.list")
+    modelscope_subparsers.add_argument("--max_seconds", type=int, default=15, help="Max sliced voice length(seconds), Default: 15")
+    modelscope_subparsers.set_defaults(func=handle_create)
+
+    whisper_subparsers = create_subparsers.add_parser('whisper', 
+                                                      help='whisper models')
+    whisper_subparsers.add_argument("--source_dir", type=str, default="origin", help="Source directory path, Default: origin")
+    whisper_subparsers.add_argument("--target_dir", type=str, default="dataset", help="Target directory path, Default: dataset")
+    whisper_subparsers.add_argument("--resample_dir", type=str, default="origin_resample", help="Resample directory path, Default: origin_resample")
+    whisper_subparsers.add_argument("--sample_rate", type=int, default=44100, help="Sample rate, Default: 44100")
+    whisper_subparsers.add_argument("--language", type=str, default="ZH", help="Language, Default: ZH")
+    whisper_subparsers.add_argument("--output", type=str, default="demo.list", help="List file, Default: demo.list")
+    whisper_subparsers.add_argument("--max_seconds", type=int, default=15, help="Max sliced voice length(seconds), Default: 15")
+    whisper_subparsers.set_defaults(func=handle_create)
 
 
     args = parser.parse_args()
-
 
     if hasattr(args, 'func'):
         args.func(args)
